9fe15851c0875ddb93030748ca5a5138ed0bcfe0
comerc <comerc@mail.ru>
2015-01-12 18:16:47 +0300

    init

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'comerc:bs-datetimepicker',
+  summary: 'Bootstrap 3 DateTimePicker-widget (v4beta)',
+  version: '1.0.0',
+  git: 'https://github.com/comerc/meteor-bs-datetimepicker.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  api.use('jquery', 'client');
+  api.use('momentjs:moment@2.8.4', 'client');
+  api.addFiles([
+    'bootstrap-datetimepicker.css',
+    'bootstrap-datetimepicker.js',
+  ], 'client');
+});
